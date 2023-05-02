46885942e84c4fbc9f71f130e1b8d0b0f5283621
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-05-02 17:59:29 +0200

    Server ok

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,52 @@
-const express = require('express');
+const express = require("express");
 
 // Création de l'application express
 const app = express();
 
+// Met à disposition content-type: application/json et met à disposition dans requ.body (Donne accès au corps de la requête)
+app.use(express.json())
+
+// Middleware qui permet de transformer le corps de la requête en objet JS utilisable
 app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-app.use((req, res, next) => {
-    res.status(201);
-    next();
+app.post("/api/stuff", (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({message : "Objet créé !"})
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !')
+app.get("/api/stuff", (req, res, next) => {
+  const stuff = [
+    {
+      _id: "oeihfzeoi",
+      title: "Mon premier objet",
+      description: "Les infos de mon premier objet",
+      imageUrl:
+        "https://images.unsplash.com/photo-1531722569936-825d3dd91b15?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      price: 4900,
+      userId: "qsomihvqios",
+    },
+    {
+      _id: "oeihfzeomoihi",
+      title: "Mon deuxième objet",
+      description: "Les infos de mon deuxième objet",
+      imageUrl:
+        "https://images.unsplash.com/photo-1586996292898-71f4036c4e07?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+      price: 2900,
+      userId: "qsomihvqios",
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 module.exports = app;
-
