9c33e87eaac71848180e8cb545c47a3210f3828d
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-05-03 16:25:15 +0200

    Router

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const stuffRoutes = require("./routes/stuff");
 
 // Création de l'application express
 const app = express();
-
-// Importation du modèle Thing
-const Thing = require("./models/thing");
 
 // Connexion à la base de données MongoDB
 mongoose
@@ -33,44 +31,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Récupération de tous les objets
-app.get("/api/stuff", (req, res, next) => {
-  Thing.find()
-    .then((things) => res.status(200).json(things))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// Récupération d'un objet
-app.get("/api/stuff/:id", (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then((thing) => res.status(200).json(thing))
-    .catch((error) => res.status(404).json({ error }));
-});
-
-//Créer un objet
-app.post("/api/stuff", (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body,
-  });
-  thing
-    .save()
-    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// Mise à jour d'un objet
-app.put("/api/stuff/:id", (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet modifié !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
-
-// Suppression d'un objet
-app.delete("/api/stuff/:id", (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-    .catch((error) => res.status(400).json({ error }));
-});
+app.use("/api/stuff", stuffRoutes);
 
 module.exports = app;
